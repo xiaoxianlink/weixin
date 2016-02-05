f1a8b4442e9df0f19997374dabc24c60f066769f
Yanger <Yanger@xiaoxianlink.com>
2016-02-05 15:54:14 +0800

    Update db.php

--- data/conf/db.php
+++ data/conf/db.php
@@ -5,10 +5,10 @@
  */
 return array(
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'chewu001.mysql.rds.aliyuncs.com',
+    'DB_HOST' => '', //需要设置HOST
     'DB_NAME' => 'chewu',
     'DB_USER' => 'chewu',
-    'DB_PWD' => 'abc123',
+    'DB_PWD' => '', //需要设置Password
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'cw_',
     //密钥
