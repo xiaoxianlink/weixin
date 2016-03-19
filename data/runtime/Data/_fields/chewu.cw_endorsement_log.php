36cf788e9c7f638c99fe594d27b0006bac1fa703
yimlin <yimlin@163.com>
2016-03-19 22:58:00 +0800

    remove those file that not need to submit to scm

--- data/runtime/Data/_fields/chewu.cw_endorsement_log.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:6:"end_id";i:2;s:5:"state";i:3;s:6:"c_time";i:4;s:4:"type";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:7:"int(11)";s:6:"end_id";s:7:"int(11)";s:5:"state";s:7:"int(11)";s:6:"c_time";s:7:"int(11)";s:4:"type";s:7:"int(11)";}}
